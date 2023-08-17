281be7ced74b02421212cee3e3e1884eea540df3
Steven Aushev <140964513+oldman-sa@users.noreply.github.com>
2023-08-17 17:13:52 +0300

    Update name.py

--- name.py
+++ name.py
@@ -5,20 +5,23 @@ from pyrogram import Client
 
 app = Client("anon", api_id=config.API_ID, api_hash=config.API_HASH)
 
+
 async def main():
-    last_time_string = ''
-    
+    last_update = 0
+
     while True:
         local_time = time.localtime()
         new_first_name = time.strftime("%H:", local_time)
         new_last_name = time.strftime("%M", local_time)
         
-        try:
-            await app.update_profile(first_name=new_first_name, last_name=new_last_name)
-        except Exception as e:
-            print(f"Error updating profile: {e}")
-        
-        await asyncio.sleep(60)
+        if last_update != new_last_name:
+            try:
+                await app.update_profile(first_name=new_first_name, last_name=new_last_name)
+                last_update = new_last_name
+            except Exception as e:
+                print(f"Error updating profile: {e}")
+
+        await asyncio.sleep(1)
 
 app.start()
 app.run(main())
